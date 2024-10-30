cb2a7bfc1128bd1bef22cbc64ff1f3831f59f0dc
biratheeban <biratheeban@gmail.com>
2024-10-30 08:51:12 +0530

    Update genkinsconnect.py

--- genkinsconnect.py
+++ genkinsconnect.py
@@ -6,23 +6,6 @@ import os
 jenkins_url = "https://jenkinsurl"
 auth = ("user", "authtoken")
 
-
-def fetch_build_history():
-    response = requests.get(jenkins_url, auth=auth, verify=False)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    print(response.content)
-    build_links = []
-    build_history_div = soup.find("div", id="buildHistoryPage")
-    if build_history_div:
-        for a_tag in build_history_div.find_all("a", class_="build-row", href=True):
-            job_number = a_tag.text.strip()
-            href = a_tag["href"]
-            full_url = f"{jenkins_url}{href}"
-            build_links.append((job_number, full_url))
-
-    return build_links
-
 def save_build_link_to_db(job_number, url):
     is_exist=false
     connection = mysql.connector.connect(db_config)
@@ -41,3 +24,19 @@ def save_build_link_to_db(job_number, url):
     cursor.close()
     connection.close()
     return is_exist
+
+def fetch_build_history():
+    response = requests.get(jenkins_url, auth=auth, verify=False)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    print(response.content)
+    build_links = []
+    build_history_div = soup.find("div", id="buildHistoryPage")
+    if build_history_div:
+        for a_tag in build_history_div.find_all("a", class_="build-row", href=True):
+            job_number = a_tag.text.strip()
+            href = a_tag["href"]
+            full_url = f"{jenkins_url}{href}"
+            build_links.append((job_number, full_url))
+
+    return build_links
